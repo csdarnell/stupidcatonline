0d83fdbd273158b7f2ec0e65fec0087462e3512b
Chris Darnell <csdarnell@gmail.com>
2020-03-22 15:45:33 -0400

    Tweaked the Page Not Found page

--- src/views/page-not-found.vue
+++ src/views/page-not-found.vue
@@ -1,16 +1,11 @@
 <template>
     <div class="content-container">
         <div class="content-title-group not-found">
-            <i class="fas fa-exclamation-triangle" aria-hidden="true">
+            <i class="fas fa-exclamation-triangle" aria-hidden="true">These aren't the Friskies you're looking for.
+            <br/>
+            <br/>
+            Page not found.
             </i>
-            &nbsp;
-            <span class="title">These aren't the Friskies you're looking for.<br/>
-            <br/>
-            Nope<br/>
-            <br/>
-            Sorry<br/>
-            <br/>
-            Can't find that page.</span>
         </div>
     </div>
 </template>
