cd2a495ab4054cbcb28fe8d74d57270b04793426
Chris Darnell <csdarnell@gmail.com>
2020-03-28 10:12:18 -0400

    Cat Food vue page now loads, but still has errors

--- src/store/index.js
+++ src/store/index.js
@@ -1,42 +1,15 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-import { dataService } from '../shared';
-import {
-    GET_CATFOOD
-} from './mutation-types';
-
-Vue.use(Vuex);
-
-const state = () => ({
-    catfood: []
-});
-
-const mutations = {
-    [GET_CATFOOD](state, catfood) {
-        console.log('index.js:GET_CATFOOD()');
-        state.catfood = catfood;
-    }
-};
-
-const actions = {
-    async getCatFoodAction({ commit }) {
-        console.log('index:getCatFoodAction()');
-        const catfood = await dataService.getCatFood();
-        commit(GET_CATFOOD, catfood);
-    }
-};
-
-const getters = {
-    // parameterized getters are not cached.  so this is just a convenience to get the state.
-    getCatFoodById: state => id => state.catfood.find(h => h.id === id)
-};
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-    strict: process.env.NODE_ENV !== 'production',
-    state,
-    mutations,
-    actions,
-    getters
-});
-
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
